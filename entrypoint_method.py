709e114f21dcec8c9b58d155d285d06f499990ba
Daniel Incicau <daniel_incicau@yahoo.com>
2024-04-19 14:47:44 +0200

    Update run.sh

--- entrypoint_method.py
+++ entrypoint_method.py
@@ -36,12 +36,23 @@ def main():
     # Add arguments
     parser.add_argument('--output_dir', type=str, help='output directory where method will store results.')
     parser.add_argument('--name', type=str, help='name of the dataset')
-    parser.add_argument('--input_files', type=str, help='input files required by the method.')
+    parser.add_argument('--process.filtered', type=str, help='optional input file #1.', required=False)
+    parser.add_argument('--data.counts', type=str, help='optional input file #1.', required=False)
+    parser.add_argument('--data.meta', type=str, help='input file #2.')
+    parser.add_argument('--data.data_specific_params', type=str, help='input file #3.')
 
     # Parse arguments
     args, extra_arguments = parser.parse_known_args()
 
-    input_files = args.input_files.split(',')
+    process_filtered_input = getattr(args, 'process.filtered')
+    data_counts_input = getattr(args, 'data.counts')
+    data_meta_input = getattr(args, 'data.meta')
+    data_params_input = getattr(args, 'data.data_specific_params')
+
+    assert process_filtered_input is not None or data_counts_input is not None, "At least one of the values must not be None"
+    data_counts_input = process_filtered_input if process_filtered_input else data_counts_input
+    
+    input_files = [data_counts_input, data_meta_input, data_params_input]
 
     run_method(args.output_dir, args.name, input_files, extra_arguments)
 
